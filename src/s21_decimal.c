30dbaa3ec7565d11e55b3a17b81d1e4884b7bedb
Pilaf Berenice <pilafber@ev-d2.kzn.21-school.ru>
2022-05-30 17:27:01 +0300

    SD-18 <Функция + тесты>

--- src/s21_decimal.c
+++ src/s21_decimal.c
@@ -144,3 +144,22 @@ int s21_is_greater(s21_decimal value_1, s21_decimal value_2) {
 
     return res;
 }
+
+int s21_is_equal(s21_decimal value_1, s21_decimal value_2) {
+    int is_equal = TRUE;
+
+    if (value_1.bits[0] == 0 && value_2.bits[0] == 0 && value_1.bits[1] == 0 &&
+        value_2.bits[1] == 0 && value_1.bits[2] == 0 && value_2.bits[2] == 0) {
+        // is_equal = True
+    } else if (value_1.bits[0] != value_2.bits[0] ||
+               value_1.bits[1] != value_2.bits[1] ||
+               value_1.bits[2] != value_2.bits[2] ||
+               value_1.bits[3] != value_2.bits[3])
+        is_equal = FALSE;
+
+    return is_equal;
+}
+
+int s21_is_greater_or_equal(s21_decimal value_1, s21_decimal value_2) {
+    return (s21_is_greater(value_1, value_2) || s21_is_equal(value_1, value_2));
+}
